68167b11a80291e22eb0f32e9572270956a3844d
j4vokhir.abdurakhmonov <j4vokhir.abdurakhmonov@yandex.ru>
2022-04-01 01:21:28 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// String
+
+let firstName = 'Javokhir';
+console.log(firstName);
+
+let surName = 'Abdurakhmonov';
+console.log(surName);
+
+// Number
+
+let age = 18;
+console.log(age);
+
+let price = 23.999;
+console.log(price);
+
+// boolean
+
+const a = 33131;
+const b = 313133;
+console.log(a == b)
+
+const c = true;
+const d = false;
+
+// Undefined
+
+let userName;
+console.log(userName)
+
+// Null
+let profession = null;
+console.log(profession);
+
+// Symbol
+
+let watch = Symbol('watch');
+console.log(watch)
+
+//  BigInt
+
+const sameBigint = BigInt("1234567890123456789012345678901234567890");
+console.log (sameBigint);
+
+//  Object
